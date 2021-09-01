f0d6d53f020f00b0cae6edcd92ebcd3875d40b73
choyon das <choyondas08@gmail.com>
2021-09-02 03:23:01 +0800

    added photo and search number

--- js/script.js
+++ js/script.js
@@ -10,13 +10,30 @@ const searchBook = () => {
     const url = `http://openlibrary.org/search.json?q=${searchResults}`
     fetch(url)
         .then(res => res.json())
-        .then(data => loadBook(data.docs))
+        .then(data => loadBook(data))
 }
-
-
-
 const loadBook = data => {
-    data.forEach(bookItem => {
-        console.log(bookItem);
+    const itemsNumber = data.numFound;
+
+    const number = document.getElementById('itemsNumber');
+    number.innerText = `Total search items = ${itemsNumber}`;
+
+
+
+    const items = data.docs;
+    items.forEach(bookItem => {
+        // console.log(bookItem);
+
+        const results = document.getElementById('searchResults');
+        const div = document.createElement('div');
+        div.classList.add('design')
+        div.innerHTML = `
+        <img width="100px" src="https://covers.openlibrary.org/b/id/${bookItem.cover_i}-L.jpg">
+<h2>${bookItem.title} </h2>
+<p> ${bookItem.author_name}</p>
+<p>Publish Year: ${bookItem.first_publish_year}</p>
+`
+        results.appendChild(div);
+
     })
 }
